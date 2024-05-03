747fbc1aa2fbdb00d81724449eddbbf9d609fcad
Paramesh2545 <152502158+Paramesh2545@users.noreply.github.com>
2024-05-03 08:12:47 +0530

    new

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { GrMapLocation } from "react-icons/gr";
+import { CiHome } from "react-icons/ci";
+import { SlBasket } from "react-icons/sl";
+import { IoMdPerson } from "react-icons/io";
+import "../styles.css";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-  return <div>navbar</div>;
+  return (
+    <div className="navmain">
+      <div className="innernav">
+        <NavLink to="/">
+          <CiHome className="navelements" />
+        </NavLink>
+        <NavLink to="/near">
+          <GrMapLocation className="navelements" />
+        </NavLink>
+        <NavLink to="/cart">
+          <SlBasket className="navelements" />
+        </NavLink>
+        <NavLink to="/profile">
+          <IoMdPerson className="navelements" />
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
